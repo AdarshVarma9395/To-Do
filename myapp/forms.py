b01f4e9ba1838fc8330d6e939c00b2439d756cab
AdarshVarma9395 <adarshvarma1999@gmail.com>
2024-05-12 20:18:31 +0530

    HTML&CSS
    Added some html and css on templates

--- myapp/forms.py
+++ myapp/forms.py
@@ -1,8 +1,10 @@
 from .models import *
 from django.forms import ModelForm
+from django import forms
 
 
 class TaskForm(ModelForm):
+    title = forms.CharField(widget=forms.TextInput(attrs={'placeholder':'Add new task...'}))
     class Meta:
         model = Task
         fields = '__all__'
